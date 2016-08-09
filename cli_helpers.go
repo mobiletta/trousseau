b92940d10ce4449fa7613dfa67f097546f352220
tcr <tcr@ableton.com>
2016-08-09 20:04:08 +0200

    refresh gopasswd usage

--- cli_helpers.go
+++ cli_helpers.go
@@ -2,10 +2,13 @@ package trousseau
 
 import (
 	"fmt"
+
 	"github.com/howeyc/gopass"
 )
 
 func PromptForPassword() string {
 	fmt.Printf("Password: ")
-	return string(gopass.GetPasswd())
+	pass, _ := gopass.GetPasswd()
+
+	return string(pass)
 }
